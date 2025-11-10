2e46d2ee944f4da5aa5f0b84f490607ad958f641
unknown <Administrator@L2-4.lgcns.com>
2025-11-10 17:39:20 +0900

    feat:회원 탈퇴 수정

--- src/utils/member.js
+++ src/utils/member.js
@@ -30,7 +30,23 @@ export async function patchMember(data){
 // 회원 탈퇴
 export async function withdrawal() {
   try {
-    const response = await api.patch("/active/update");
+    console.log("탈퇴 함수 실행");
+    
+    // sessionStorage에서 카카오 토큰 가져오기
+    const kakaoToken = sessionStorage.getItem("kakaoToken");
+
+    const response = await api.patch(
+      "/active/update",
+      { kakaoToken }, // 백엔드에서 unlink 처리용
+      { withCredentials: true } // 쿠키 포함
+    );
+
+    if (response.data.success) {
+      // JWT 등도 정리
+      localStorage.removeItem("token");
+      sessionStorage.removeItem("kakaoToken");
+    }
+
     const success = response.data.success;
     console.log(success);
     return success;
